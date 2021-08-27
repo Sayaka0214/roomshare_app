e7033e1ea131fc9efced9ee866576b46649026d2
EC2 Default User <ec2-user@ip-172-31-18-163.us-east-2.compute.internal>
2021-08-27 11:51:06 +0000

    ユーザー・ルーム実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  # homeコントローラー作成時自動生成↓
+  get 'home/index'
+  get 'home/show' 
+  # 追記（多分ルートパス）
+  root to: "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
